1dc54ba5ce7348f92a758f1d4b7708e0df1ffa89
ReZoneStudio <93032631+ReZoneStudio@users.noreply.github.com>
2022-04-30 22:30:32 +0430

    Update source.lua

--- source.lua
+++ source.lua
@@ -58,8 +58,8 @@ local themes = {
 local themeStyles = {
 	DarkTheme = {
 		SchemeColor = Color3.fromRGB(64, 64, 64),
-		Background = Color3.fromRGB(43, 43, 43),
-		Header = Color3.fromRGB(29, 29, 29),
+		Background = Color3.fromRGB(45, 45, 45),
+		Header = Color3.fromRGB(35, 35, 35),
 		TextColor = Color3.fromRGB(255,255,255),
 		ElementColor = Color3.fromRGB(20, 20, 20)
 	},
@@ -265,7 +265,7 @@ function Kavo.CreateLib(kavName, themeList)
 	title.Font = Enum.Font.Gotham
 	title.RichText = true
 	title.Text = kavName
-	title.TextColor3 = Color3.fromRGB(245, 245, 245)
+	title.TextColor3 = Color3.fromRGB(255, 115, 0)
 	title.TextSize = 14.000
 	title.TextXAlignment = Enum.TextXAlignment.Left
 
