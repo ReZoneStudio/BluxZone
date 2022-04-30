9191b14a37be16544409dfc57b64a842793d28f7
ReZoneStudio <93032631+ReZoneStudio@users.noreply.github.com>
2022-04-30 22:25:58 +0430

    Update source.lua

--- source.lua
+++ source.lua
@@ -58,9 +58,9 @@ local themes = {
 local themeStyles = {
 	DarkTheme = {
 		SchemeColor = Color3.fromRGB(64, 64, 64),
-		Background = Color3.fromRGB(45, 45, 45),
-		Header = Color3.fromRGB(35, 35, 35),
-		TextColor = Color3.fromRGB(1,1,1),
+		Background = Color3.fromRGB(43, 43, 43),
+		Header = Color3.fromRGB(29, 29, 29),
+		TextColor = Color3.fromRGB(255,255,255),
 		ElementColor = Color3.fromRGB(20, 20, 20)
 	},
 	LightTheme = {
